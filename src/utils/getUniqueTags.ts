af07866a5a607274f5e65857238f757f07f54fea
Joekma <596014054@qq.com>
2026-05-10 11:00:02 +0800

    Enhance blog navigation and tag browsing

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -5,19 +5,30 @@ import postFilter from "./postFilter";
 interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
+  const tagMap = new Map<string, Tag>();
+
+  posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
+    .forEach(tagName => {
+      const tag = slugifyStr(tagName);
+      const currentTag = tagMap.get(tag);
+
+      tagMap.set(tag, {
+        tag,
+        tagName,
+        count: currentTag ? currentTag.count + 1 : 1,
+      });
+    });
+
+  return Array.from(tagMap.values()).sort(
+    (tagA, tagB) =>
+      tagB.count - tagA.count || tagA.tag.localeCompare(tagB.tag)
+  );
 };
 
 export default getUniqueTags;
